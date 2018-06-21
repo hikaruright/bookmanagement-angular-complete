0fd0fa6fdb0694f37c493f838a2d5313cf0ad59b
Hikaru Sato <hisato@solxyz.co.jp>
2018-06-21 13:50:29 +0900

    router設定多分終わり

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { RouterModule, Routes, Router } from '@angular/router';
+import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}, //404 page
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes), // RouterModule
+    TooltipModule.forRoot() //Bootstrapを使用
   ],
   providers: [],
   bootstrap: [AppComponent]
