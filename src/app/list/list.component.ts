81c3d2cdefc6be42b42d75327536113eee6b1331
Hikaru Sato <hisato@solxyz.co.jp>
2018-06-27 15:28:44 +0900

    リスト処理の追加

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpConst } from '../../logic/http-const';
+import { SessionManager } from '../../logic/session-manager';
 
 @Component({
   selector: 'app-list',
@@ -13,28 +16,55 @@ export class ListComponent implements OnInit {
 
   books: BookListDto[];
 
-  constructor(public fb: FormBuilder) {
-    this.createForm();
+  constructor(private http: HttpClient) {
+    this.searchModel = new SearchCondition();
    }
 
   ngOnInit() {
+
+    this.search(null);
+
   }
 
-  createForm() {
-    this.searchForm = this.fb.group({
-      title: '',
-      author: '',
-      publisher: '',
-    });
-  }
-
+  /**
+   * 検索処理
+   * @param $event イベント
+   */
   search($event) {
+
+    if(!this.searchModel.title 
+      && !this.searchModel.author 
+      && !this.searchModel.publisher) {
+
+        // 検索内容がない場合、全件を取得する。
+
+        // ヘッダにx-access-tokenをつけて送信する
+        this.http.get<BookListDto[]>(HttpConst.url("/books"), {
+          headers: SessionManager.requestHeader()
+        }).subscribe((list) => {
+            this.books = list;
+          });
+    }else {
+
+      this.http.post<BookListDto[]>(HttpConst.url("/books"), {
+        title: this.searchModel.title,
+        author: this.searchModel.author,
+        publisher: this.searchModel.publisher
+      }, { headers: SessionManager.requestHeader()})
+        .subscribe((list) => {
+          this.books = list;
+        });
+    }
   }
 
   clicked($event, book: BookListDto) {
 
     let id = book.id;
     alert(id);
+  }
+
+  create() {
+    location.href = "/new";
   }
 
 }
