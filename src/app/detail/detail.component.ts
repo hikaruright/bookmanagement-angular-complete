abd384aa58b75c01a75a0263a5b91097ac42a5f1
Hikaru Sato <hisato@solxyz.co.jp>
2018-06-27 16:37:01 +0900

    詳細画面追加

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BookModel } from '../../models/book';
+import { ActivatedRoute } from '@angular/router';
+import { HttpConst } from '../../logic/http-const';
+import { SessionManager } from '../../logic/session-manager';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  // 表示モデル
+  model: BookModel;
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) { 
+    this.model = new BookModel();
+  }
 
   ngOnInit() {
+    // 書籍情報を取得する
+    let sub = this.route.params.subscribe(params => {
+      this.id = params["id"];
+
+      // httpで取得する
+      this.http.get<BookModel>(HttpConst.url("/book/"+this.id),{headers: SessionManager.requestHeader()})
+        .subscribe(data => {
+          this.model = data;
+        });
+    });
   }
 
 }
