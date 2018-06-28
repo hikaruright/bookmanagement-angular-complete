8dbeab617885f38fa476378434412c563531fb7d
Hikaru Sato <hisato@solxyz.co.jp>
2018-06-28 16:14:46 +0900

    編集画面の初期表示実装

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BookModel } from '../../models/book';
+import { ListModel } from '../../models/listmodel';
+import { HttpClient } from '@angular/common/http';
+import { HttpConst } from '../../logic/http-const';
+import { SessionManager } from '../../logic/session-manager';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  model: BookModel;
+
+  publishers: ListModel[];
+
+  departments: ListModel[];
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.model = new BookModel();
+
+  }
 
   ngOnInit() {
+    let sub = this.route.params.subscribe(params => {
+      this.id = params["id"];
+
+      // httpで取得する
+      this.http.get<BookModel>(HttpConst.url("/book/" + this.id), { headers: SessionManager.requestHeader() })
+        .subscribe(data => {
+          this.model = data;
+        });
+    });
+    // 出版社一覧をセット
+    this.http.get<ListModel[]>(HttpConst.url("/list/publisher"),
+      { headers: SessionManager.requestHeader() })
+      .subscribe(result => {
+        this.publishers = result;
+        //this.model.publisher = result[0].id;
+      });
+
+    // 部署一覧をセット
+    this.http.get<ListModel[]>(HttpConst.url("/list/department"),
+      { headers: SessionManager.requestHeader() })
+      .subscribe(result => {
+        this.departments = result;
+        //this.model.managedDpt = result[0].id;
+      })
+    // httpで取得する
+    this.http.get<BookModel>(HttpConst.url("/book/" + this.id), { headers: SessionManager.requestHeader() })
+      .subscribe(data => {
+        this.model = data;
+      });
   }
 
 }
