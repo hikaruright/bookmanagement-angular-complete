e7b47435413747f351ce8afc21ad8098ec37aaa9
Hikaru Sato <hisato@solxyz.co.jp>
2018-07-04 16:10:20 +0900

    問題用に中身を削る

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -27,60 +27,6 @@ export class EditComponent implements OnInit {
   }
 
   ngOnInit() {
-    let sub = this.route.params.subscribe(params => {
-      this.id = params["id"];
-
-      // httpで取得する
-      this.http.get<BookModel>(HttpConst.url("/book/" + this.id), { headers: SessionManager.requestHeader() })
-        .subscribe(data => {
-          this.model = data;
-          this.model.id = this.id;
-        });
-    });
-    // 出版社一覧をセット
-    this.http.get<ListModel[]>(HttpConst.url("/list/publisher"),
-      { headers: SessionManager.requestHeader() })
-      .subscribe(result => {
-        this.publishers = result;
-        //this.model.publisher = result[0].id;
-      });
-
-    // 部署一覧をセット
-    this.http.get<ListModel[]>(HttpConst.url("/list/department"),
-      { headers: SessionManager.requestHeader() })
-      .subscribe(result => {
-        this.departments = result;
-        //this.model.managedDpt = result[0].id;
-      })
-    // httpで取得する
-    this.http.get<BookModel>(HttpConst.url("/book/" + this.id), { headers: SessionManager.requestHeader() })
-      .subscribe(data => {
-        this.model = data;
-      });
-  }
-
-  /**
-   * 登録処理
-   */
-  register() {
-    //alert(this.model.id + "?" + this.id);
-    if(confirm("登録します。よろしいですか？")) {
-      this.model.id = this.id;
-      this.http.post(HttpConst.url("/book"), 
-        this.model, 
-        {headers: SessionManager.requestHeader()})
-        .subscribe(result => {
-          if(!result["result"]) {
-            alert("サーバにてエラーが発生しました。");
-          } else {
-            alert ("登録に成功しました。");
-            let id = result["id"];
-
-            location.href="/detail/"+id;
-          }
-        });
-    }
     
   }
-
 }
