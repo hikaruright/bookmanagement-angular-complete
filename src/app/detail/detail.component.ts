351bdc26c5768dc1ad7dd014eef7b1ea11075b1f
Hikaru Sato <hisato@solxyz.co.jp>
2018-07-04 16:19:42 +0900

    コメント諸々追加

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -5,6 +5,9 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpConst } from '../../logic/http-const';
 import { SessionManager } from '../../logic/session-manager';
 
+/**
+ * 詳細表示コンポーネント
+ */
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
@@ -12,11 +15,17 @@ import { SessionManager } from '../../logic/session-manager';
 })
 export class DetailComponent implements OnInit {
 
+  /** 表示対象となるID */
   id: string;
 
-  // 表示モデル
+  /** 表示する書籍情報 */
   model: BookModel;
 
+  /**
+   * コンストラクタ
+   * @param http HTTP通信モジュール
+   * @param route ルーター情報
+   */
   constructor(private http: HttpClient, private route: ActivatedRoute) {
     this.model = new BookModel();
   }
@@ -24,6 +33,7 @@ export class DetailComponent implements OnInit {
   ngOnInit() {
     // URLパラメータから取得するIDを取得
     this.route.params.subscribe(params => {
+      // id情報を取得する。
       this.id = params["id"];
       // 書籍情報を取得する
       this.http.get<BookModel>(
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,17 +11,26 @@ import { SessionManager } from '../../logic/session-manager';
 })
 export class ListComponent implements OnInit {
 
+  /**検索条件 */
   @Input() searchModel: SearchCondition;
   searchForm: FormGroup;
 
+  /** 取得した書籍の一覧情報 */
   books: BookListDto[];
 
+  /**
+   * コンストラクタ
+   * @param http HTTP通信モジュール
+   */
   constructor(private http: HttpClient) {
     this.searchModel = new SearchCondition();
-   }
+  }
 
+  /**
+   * 画面ロード時のイベント
+   */
   ngOnInit() {
-
+    // 条件なしで検索を行う
     this.search(null);
 
   }
@@ -32,25 +41,25 @@ export class ListComponent implements OnInit {
    */
   search($event) {
 
-    if(!this.searchModel.title 
-      && !this.searchModel.author 
+    if (!this.searchModel.title
+      && !this.searchModel.author
       && !this.searchModel.publisher) {
 
-        // 検索内容がない場合、全件を取得する。
+      // 検索内容がない場合、全件を取得する。
 
-        // ヘッダにx-access-tokenをつけて送信する
-        this.http.get<BookListDto[]>(HttpConst.url("/books"), {
-          headers: SessionManager.requestHeader()
-        }).subscribe((list) => {
-            this.books = list;
-          });
-    }else {
+      // ヘッダにx-access-tokenをつけて送信する
+      this.http.get<BookListDto[]>(HttpConst.url("/books"), {
+        headers: SessionManager.requestHeader()
+      }).subscribe((list) => {
+        this.books = list;
+      });
+    } else {
 
       this.http.post<BookListDto[]>(HttpConst.url("/books"), {
         title: this.searchModel.title,
         author: this.searchModel.author,
         publisher: this.searchModel.publisher
-      }, { headers: SessionManager.requestHeader()})
+      }, { headers: SessionManager.requestHeader() })
         .subscribe((list) => {
           this.books = list;
         });
@@ -61,20 +70,36 @@ export class ListComponent implements OnInit {
 
     let id = book.id;
     //alert(id);
-    location.href="/detail/"+id;
+    location.href = "/detail/" + id;
   }
 
 
 }
 
+/**
+ * 検索条件
+ */
 class SearchCondition {
+  /** タイトル */
   public title: string;
+  /** 著者 */
   public author: string;
+  /** 出版社*/
   public publisher: string;
 }
 
+/**
+ * 一覧に表示する書籍情報
+ */
 class BookListDto {
 
+  /**
+   * コンストラクタ
+   * @param id ID
+   * @param title タイトル
+   * @param author 著者
+   * @param publisher 出版社
+   */
   constructor(
     public id: string,
     public title: string,
