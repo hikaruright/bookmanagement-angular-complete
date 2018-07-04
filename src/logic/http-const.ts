25cd65c17eeb05018b700679300ade67037b9777
Hikaru Sato <hisato@solxyz.co.jp>
2018-07-04 15:18:13 +0900

    コメント追加

--- src/logic/http-const.ts
+++ src/logic/http-const.ts
@@ -1,7 +1,17 @@
+/**
+ * Http系の定数クラス
+ */
 export class HttpConst {
 
+    /**
+     * サーバのエンドポイント
+     */
     public static endpoint = 'http://localhost:3000';
 
+    /**
+     * URLを返却
+     * @param path パス
+     */
     public static url(path: string) {
         return HttpConst.endpoint + path;
     }
